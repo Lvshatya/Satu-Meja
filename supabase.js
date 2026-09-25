b5e262ba8e61f57c4a0314e6f276bd9b7bbce23d
Lvshatya <elfishasatya@gmail.com>
2026-09-25 18:55:08 +0700

    Fix Supabase client global scope

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = 'https://fxlhezgwkvdmbwwvttrt.supabase.co';
 const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_aA6MV2etwACD3KAtGIHiNg_tJZy3QQ2';
 
-const supabaseClient = window.supabase.createClient(
+window.supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_PUBLISHABLE_KEY
 );
